ff45269e7482e1430427836fbf245caf016aa088
bryanelmercahyadi <75104250+bryanelmercahyadi@users.noreply.github.com>
2021-08-11 23:43:01 +0800

    ucapan done

--- resources/views/ucapan/foto.blade.php
+++ resources/views/ucapan/foto.blade.php
@@ -120,6 +120,38 @@
     }
     /* ============================================ */
 
+    /* HAFT MODAL */
+    .haft-header-modal{
+        width: 100%;
+        background-color: #1B325E !important;
+        color:#fff !important;
+        font-weight: bold !important;
+        height: 65px !important;
+        font-size: 14pt !important;
+        
+    }
+    .haft-modal-btn{
+        cursor: pointer;
+        background-color: #D02762!important;
+        border: 2px solid white !important;
+        color: white !important;
+        border-radius: 20px !important;
+        min-width: 80px !important;
+        min-height: 45px !important;
+    }
+    .haft-modal-btn:hover{
+        cursor: pointer;
+        background-color: white !important;
+        border: 2px solid #D02762 !important;
+        color: #D02762 !important;
+        border-radius: 20px !important;
+        
+    }
+    
+
+
+    /* =================== */
+
 
 
     @media only screen and (max-width: 1200px) {
@@ -147,7 +179,7 @@
             </div>
             <div class="col-3"></div>
             <div class="col-3 haft-btn-pink">
-                <p class="text-center pt-2">Unggah Fotomu</p>
+                <p class="text-center pt-2" data-bs-toggle="modal" data-bs-target="#modal-ucapan">Unggah Fotomu</p>
             </div>
         </div>
         <div class="row line-pink">
@@ -681,6 +713,149 @@
         <div class="row pt-5 pb-5"></div>
         
 
+    </div>
+
+    {{-- Modal Ucapan --}}
+    <div class="modal fade haft-modal" id="modal-ucapan" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="row p-0 m-0 pt-3 ps-3 haft-header-modal">
+                <div class="col">
+                    <h3 style="">Unggah Ucapanmu !</h3>
+                </div>
+            </div>
+            <form action="#" method="post" enctype="multipart/form-data" id="Msg_Form">
+                @csrf
+                <div class="row haft-form">
+                    <div class="col">
+                        <div class="row pt-3">
+                            <div class="col-1"></div>
+                            <div class="col-10">
+                                <label for="name" class="form-label">Nama Lengkap : </label>
+                                <input type="text" class="form-control" id="name" name="name"placeholder="Nama Lengkap">
+                            </div>
+                            <div class="col-1"></div>
+                        </div>
+                        <div class="row pt-3">
+                            <div class="col-1"></div>
+                            <div class="col-10">
+                                <div class="form-group" id="formEmail">
+                                    <label for="inputEmail">Email </label>
+                                    <input type="text" class="form-control" name="email" id="inputEmail" placeholder="exxxxx@email.com">
+                                </div>
+                            </div>
+                            <div class="col-1"></div>
+                        </div>
+                        <div class="row pt-3">
+                            <div class="col-1"></div>
+                            <div class="col-10">
+                                <div class="form-group">
+                                    <label class="my-1 mr-2" for="kategori">Kategori</label>
+                                    <select class="custom-select my-1 mr-sm-2" id="kategori" name="kategori" aria-placeholder="" required>
+                                        <option hidden value="0">Kategori</option>
+                                        <option value="1">Mahasiswa</option>
+                                        <option value="2">Dosen/Tenaga Kependidikan</option>
+                                        <option value="3">Alumni</option>
+                                        <option value="4">YPTK Petra</option>
+                                        <option value="5">Umum</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-1"></div>
+                        </div>
+                        <div class="row pt-3" id="formKategori2" hidden>
+                            <div class="col-1"></div>
+                            <div class="col-10">
+                                <div class="form-group">
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="detail1" id="inlineRadio1" value="Prodi/Program" checked>
+                                        <label class="form-check-label" for="inlineRadio1">Prodi/Program</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="detail1" id="inlineRadio2" value="Biro/Unit">
+                                        <label class="form-check-label" for="inlineRadio2">Biro/Unit</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-1"></div>
+                        </div>
+                        <div class="row pt-3" id="formKategoriinput2" hidden>
+                            <div class="col-1"></div>
+                            <div class="col-10">
+                                <div class="form-group" >
+                                    <label for="inputdetail2" id="labelinputdetail2">Prodi/Program</label>
+                                    <input type="text" class="form-control" name="detail2" id="inputdetail2" placeholder="Masukkan Prodi/Program">
+                                </div>
+                            </div>
+                            <div class="col-1"></div>
+                        </div>
+                        <div class="row pt-3" id="formJurusan" hidden>
+                            <div class="col-1"></div>
+                            <div class="col-10">
+                                <div class="form-group" >
+                                    <label for="inputJurusan">Prodi/Program</label>
+                                    <input type="text" class="form-control" name="jurusan" id="inputJurusan" placeholder="Jurusan">
+                                </div>
+                            </div>
+                            <div class="col-1"></div>
+                        </div>
+                        <div class="row pt-3" id="formAngkatan" hidden>
+                            <div class="col-1"></div>
+                            <div class="col-10">
+                                <div class="form-group" >
+                                    <label for="inputAngkatan">Angkatan</label>
+                                    <input type="number" class="form-control" name="angkatan" id="inputAngkatan"
+                                        placeholder="Angkatan (Contoh &quot;2018&quot;)" min="1961" max="2020">
+                                </div>
+                            </div>
+                            <div class="col-1"></div>
+                        </div>
+                        <div class="row pt-3">
+                            <div class="col-1"></div>
+                            <div class="col-10">
+                                <div class="form-group">
+                                    <label for="inputKarya">Ucapan Dalam Bentuk Foto (maks. 10MB Orientasi Landscape)</label>
+                                    <div class="input-group mb-3">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01" name="file">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-1"></div>
+                        </div>
+                        <div class="row pt-3">
+                            <div class="col-1"></div>
+                            <div class="col-10">
+                                <div class="form-group" id="formJudulMsg">
+                                    <label for="notes">Judul Foto (Max 50 Characters)</label>
+                                    <textarea class="form-control" name="judulMsg" id="inputjudulMsg" rows="3" placeholder="Judul" maxlength="50"></textarea>
+                                </div>
+                            </div>
+                            <div class="col-1"></div>
+                        </div>
+                        <div class="row pt-3">
+                            <div class="col-1"></div>
+                            <div class="col-10">
+                                <div class="form-group" id="formMsg">
+                                    <label for="notes">Ucapan Dalam Bentuk Teks (Max 400 Characters)</label>
+                                    <textarea class="form-control" name="msg" id="inputMsg" rows="3" placeholder="Message" minlength="5" maxlength="400" required></textarea>
+                                </div>
+                            </div>
+                            <div class="col-1"></div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            <div class="row pt-2 pb-5">
+                <div class="col text-center">
+                    <button class="btn haft-modal-btn submit-ucapan-foto" data-bs-dismiss="modal">
+                        <p style="margin: 0; font-size:15pt">Unggah</p>
+                    </button>
+                </div>
+            </div>
+          </div>
+        </div>
     </div>
 
 </div>
