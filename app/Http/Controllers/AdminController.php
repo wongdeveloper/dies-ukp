fc960b49d2265b60a15fb30d2019db311138d983
michaus-coder <c14190168@john.petra.ac.id>
2021-08-22 16:57:09 +0700

    admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -17,11 +17,6 @@ class AdminController extends Controller
         return view('admin.gate');
     }
 
-    // public function login(Request $request)
-    // {
-    //     dd($request);
-    // }
-
     public function wish_index()
     {
         $wish_vip = Wish::where('is_vip', '1')->get();
@@ -35,18 +30,116 @@ class AdminController extends Controller
 
     public function wish_store(StoreWishRequest $request)
     {
+        $detail1 = null;
+        $detail2 = null;
+        $image = null;
+        $video = null;
+        if ($request->is_vip == 1) {
+            $detail1 = $request->detail1;
+            $detail2 = $request->detail2;
+        }else{
+            switch ($request->kategori) {
+                case '1':
+                case '3':
+                    $detail1 = $request->jurusan;
+                    $detail2 = $request->angkatan;
+                    break;
+                case '2':
+                    $detail1 = $request->detail1;
+                    $detail2 = $request->detail2;
+                    break;
+            }
+        }
 
+        if ($request->hasFile('image')) {
+            $image_name = uniqid() . '.' . $request->image->extension();
+            $request->image->move(public_path('assets/images/wishes'), $image_name);
+            $image_path = asset('assets/images/wishes/' . $image_name);
+            $image = Image::create([
+                'path' => $image_path
+            ]);
+        }
+
+        if (isset($request->video)) {
+            $video = Video::create([
+                'path' => $request->video
+            ]);
+        }
+
+        $wish = Wish::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'role_id' => $request->kategori,
+            'detail1' => $detail1,
+            'detail2' => $detail2,
+            'wish' => $request->wish,
+            'image_id' => !is_null($image) ? $image->id : null,
+            'image_title' => isset($request->image_title) ? $request->image_title : null,
+            'video_id' => !is_null($video) ? $video->id : null,
+            'video_title' => isset($request->video_title) ? $request->video_title : null,
+            'is_vip' => $request->is_vip
+        ]);
+        return back()->with('success', 'Ucapan anda berhasil ditambahkan');
     }
 
     public function wish_edit(int $id)
     {
-
-        // return view('admin.edit.teks');
+        $wish_vip = Wish::findOrFail($id);
+        return view('admin.edit.teks', compact('wish_vip'));
     }
 
     public function wish_update(UpdateWishRequest $request)
     {
+        $detail1 = null;
+        $detail2 = null;
+        $image = null;
+        $video = null;
+        if ($request->is_vip == 1) {
+            $detail1 = $request->detail1;
+            $detail2 = $request->detail2;
+        } else {
+            switch ($request->kategori) {
+                case '1':
+                case '3':
+                    $detail1 = $request->jurusan;
+                    $detail2 = $request->angkatan;
+                    break;
+                case '2':
+                    $detail1 = $request->detail1;
+                    $detail2 = $request->detail2;
+                    break;
+            }
+        }
 
+        if ($request->hasFile('image')) {
+            $image_name = uniqid() . '.' . $request->image->extension();
+            $request->image->move(public_path('assets/images/wishes'), $image_name);
+            $image_path = asset('assets/images/wishes/' . $image_name);
+            $image = Image::create([
+                'path' => $image_path
+            ]);
+        }
+
+        if (isset($request->video)) {
+            $video = Video::create([
+                'path' => $request->video
+            ]);
+        }
+
+        $wish = Wish::findOrFail($request->wid)->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'role_id' => $request->kategori,
+            'detail1' => $detail1,
+            'detail2' => $detail2,
+            'wish' => $request->wish,
+            'image_id' => !is_null($image) ? $image->id : null,
+            'image_title' => isset($request->image_title) ? $request->image_title : null,
+            'video_id' => !is_null($video) ? $video->id : null,
+            'video_title' => isset($request->video_title) ? $request->video_title : null,
+            'is_vip' => $request->is_vip
+        ]);
+        return back()->with('success', 'Ucapan anda berhasil diubah');
     }
 
     public function wish_destroy(int $id)
@@ -57,15 +150,14 @@ class AdminController extends Controller
 
     public function photo_index()
     {
-
-        return view('admin.login.foto');
+        $wish_images = Wish::where('image_id', '!=', 'NULL')->whereNull('video_id')->get();
+        return view('admin.login.foto', compact('wish_images'));
     }
 
-    public function photo_create()
-    {
-
-        // return view('admin.create.foto');
-    }
+    // public function photo_create()
+    // {
+    //     return view('admin.create.foto');
+    // }
 
     // public function photo_store(StoreWishRequest $request)
     // {
@@ -74,8 +166,8 @@ class AdminController extends Controller
 
     public function photo_edit(int $id)
     {
-
-        // return view('admin.edit.foto');
+        $wish_image = Wish::findOrFail($id);
+        return view('admin.edit.foto', compact('wish_image'));
     }
 
     // public function photo_update(UpdateWishRequest $request)
@@ -90,14 +182,17 @@ class AdminController extends Controller
 
     public function video_index()
     {
-
-        return view('admin.login.video');
+        $wish_videos = Wish::where('video_id', '!=', 'NULL')->whereNull('image_id')->get();
+        foreach ($wish_videos as $key => $wish_video) {
+            parse_str(parse_url($wish_video->video->path, PHP_URL_QUERY), $my_array_of_vars);
+            $wish_video->youtube_id = $my_array_of_vars['v'];
+        }
+        return view('admin.login.video', compact('wish_videos'));
     }
 
     public function video_create()
     {
-
-        // return view('admin.create.video');
+        return view('admin.create.video');
     }
 
     // public function video_store(StoreWishRequest $request)
@@ -107,8 +202,8 @@ class AdminController extends Controller
 
     public function video_edit(int $id)
     {
-
-        // return view('admin.edit.video');
+        $wish_video = Wish::findOrFail($id);
+        return view('admin.edit.video', compact('wish_video'));
     }
 
     // public function video_update(UpdateWishRequest $request)
@@ -121,14 +216,14 @@ class AdminController extends Controller
 
     public function general_index()
     {
-
-        return view('admin.login.general');
+        $wishes = Wish::whereNull('video_id')->whereNull('image_id')->where('is_vip', 0)->get();
+        return view('admin.login.general', compact('wishes'));
     }
 
     public function general_create()
     {
 
-        // return view('admin.create.general');
+        return view('admin.create.general');
     }
 
     // public function general_store(StoreWishRequest $request)
@@ -137,8 +232,8 @@ class AdminController extends Controller
 
     public function general_edit(int $id)
     {
-
-        // return view('admin.edit.general');
+        $wish = Wish::findOrFail($id);
+        return view('admin.edit.general', compact('wish'));
     }
 
     // public function general_update(UpdateWishRequest $request)
